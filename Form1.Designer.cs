c4ce58c1f3723e6dfac6c24e2eb310e987546fc9
dop3t <70995020+dop3t@users.noreply.github.com>
2020-10-12 08:00:59 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Xen_Roleplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelmain = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelmenulogo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonconnect = new System.Windows.Forms.Button();
+            this.buttondiscord = new System.Windows.Forms.Button();
+            this.buttonrules = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonwebsite = new System.Windows.Forms.Button();
+            this.buttondonate = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelsidemenu = new System.Windows.Forms.Panel();
+            this.panelmain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panelmenulogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelsidemenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelmain
+            // 
+            this.panelmain.BackColor = System.Drawing.Color.Black;
+            this.panelmain.Controls.Add(this.textBox2);
+            this.panelmain.Controls.Add(this.textBox7);
+            this.panelmain.Controls.Add(this.button3);
+            this.panelmain.Controls.Add(this.textBox6);
+            this.panelmain.Controls.Add(this.button2);
+            this.panelmain.Controls.Add(this.textBox5);
+            this.panelmain.Controls.Add(this.pictureBox3);
+            this.panelmain.Controls.Add(this.textBox3);
+            this.panelmain.Controls.Add(this.textBox1);
+            this.panelmain.Controls.Add(this.richTextBox1);
+            this.panelmain.Controls.Add(this.panel2);
+            resources.ApplyResources(this.panelmain, "panelmain");
+            this.panelmain.Name = "panelmain";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.Black;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.ForeColor = System.Drawing.Color.Transparent;
+            this.textBox7.Name = "textBox7";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 2;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Black;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.ForeColor = System.Drawing.Color.Transparent;
+            this.textBox6.Name = "textBox6";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.ForeColor = System.Drawing.Color.Transparent;
+            this.textBox5.Name = "textBox5";
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.textBox1.Name = "textBox1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // panelmenulogo
+            // 
+            this.panelmenulogo.Controls.Add(this.panel1);
+            this.panelmenulogo.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.panelmenulogo, "panelmenulogo");
+            this.panelmenulogo.Name = "panelmenulogo";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonconnect
+            // 
+            this.buttonconnect.BackColor = System.Drawing.Color.Transparent;
+            this.buttonconnect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonconnect.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonconnect.FlatAppearance.BorderSize = 2;
+            this.buttonconnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonconnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.buttonconnect, "buttonconnect");
+            this.buttonconnect.ForeColor = System.Drawing.Color.White;
+            this.buttonconnect.Name = "buttonconnect";
+            this.buttonconnect.UseVisualStyleBackColor = false;
+            this.buttonconnect.Click += new System.EventHandler(this.buttonconnect_Click);
+            // 
+            // buttondiscord
+            // 
+            this.buttondiscord.BackColor = System.Drawing.Color.Black;
+            this.buttondiscord.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttondiscord.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttondiscord.FlatAppearance.BorderSize = 2;
+            this.buttondiscord.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondiscord.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.buttondiscord, "buttondiscord");
+            this.buttondiscord.ForeColor = System.Drawing.Color.White;
+            this.buttondiscord.Name = "buttondiscord";
+            this.buttondiscord.UseVisualStyleBackColor = false;
+            this.buttondiscord.Click += new System.EventHandler(this.buttondiscord_Click);
+            // 
+            // buttonrules
+            // 
+            this.buttonrules.BackColor = System.Drawing.Color.Transparent;
+            this.buttonrules.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonrules.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonrules.FlatAppearance.BorderSize = 2;
+            this.buttonrules.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonrules.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.buttonrules, "buttonrules");
+            this.buttonrules.ForeColor = System.Drawing.Color.White;
+            this.buttonrules.Name = "buttonrules";
+            this.buttonrules.UseVisualStyleBackColor = false;
+            this.buttonrules.Click += new System.EventHandler(this.buttonrules_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // buttonwebsite
+            // 
+            this.buttonwebsite.BackColor = System.Drawing.Color.Black;
+            this.buttonwebsite.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonwebsite.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonwebsite.FlatAppearance.BorderSize = 2;
+            this.buttonwebsite.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonwebsite.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.buttonwebsite, "buttonwebsite");
+            this.buttonwebsite.ForeColor = System.Drawing.Color.White;
+            this.buttonwebsite.Name = "buttonwebsite";
+            this.buttonwebsite.UseVisualStyleBackColor = false;
+            this.buttonwebsite.Click += new System.EventHandler(this.buttonwebsite_Click);
+            // 
+            // buttondonate
+            // 
+            this.buttondonate.BackColor = System.Drawing.Color.Black;
+            this.buttondonate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttondonate.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttondonate.FlatAppearance.BorderSize = 2;
+            this.buttondonate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttondonate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.buttondonate, "buttondonate");
+            this.buttondonate.ForeColor = System.Drawing.Color.White;
+            this.buttondonate.Name = "buttondonate";
+            this.buttondonate.UseVisualStyleBackColor = false;
+            this.buttondonate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panelsidemenu
+            // 
+            this.panelsidemenu.BackColor = System.Drawing.Color.Black;
+            this.panelsidemenu.Controls.Add(this.button1);
+            this.panelsidemenu.Controls.Add(this.textBox4);
+            this.panelsidemenu.Controls.Add(this.buttondonate);
+            this.panelsidemenu.Controls.Add(this.buttonwebsite);
+            this.panelsidemenu.Controls.Add(this.panel3);
+            this.panelsidemenu.Controls.Add(this.buttonrules);
+            this.panelsidemenu.Controls.Add(this.buttondiscord);
+            this.panelsidemenu.Controls.Add(this.buttonconnect);
+            this.panelsidemenu.Controls.Add(this.panelmenulogo);
+            resources.ApplyResources(this.panelsidemenu, "panelsidemenu");
+            this.panelsidemenu.Name = "panelsidemenu";
+            this.panelsidemenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelsidemenu_Paint);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelmain);
+            this.Controls.Add(this.panelsidemenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelmain.ResumeLayout(false);
+            this.panelmain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panelmenulogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelsidemenu.ResumeLayout(false);
+            this.panelsidemenu.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelmain;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panelmenulogo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonconnect;
+        private System.Windows.Forms.Button buttondiscord;
+        private System.Windows.Forms.Button buttonrules;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonwebsite;
+        private System.Windows.Forms.Button buttondonate;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelsidemenu;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xen_Roleplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void panelsidemenu_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void buttonconnect_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("fivem://connect/grandtheftauto.stateofindonesia.id"); 
+        }
+
+        private void buttonrules_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/document/d/1OBcquvhKCHpwwe0aWJT5PlETrtXuNN2b6NaSaS2vDdA/edit?usp=sharing");
+        }
+
+        private void buttondiscord_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/vyzuNk7");
+        }
+
+        private void buttondonate_Click(object sender, EventArgs e)
+        {
+             System.Diagnostics.Process.Start("fivem://connect/139.99.24.160:30122");
+             System.Diagnostics.Process.Start("ts3server://soigta.gq"); 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonwebsite_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("fivem://connect/139.99.24.160:30122");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ts3server://soigta.gq:9987");
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
